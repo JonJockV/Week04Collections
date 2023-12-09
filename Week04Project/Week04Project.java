96a1d381b1a9188b3fcc9fc968ea798552544b4a
Ben Karl <janimkarl@gmail.com>
2023-12-09 00:59:51 -0700

    tester commit

--- Week04Project/Week04Project.java
+++ Week04Project/Week04Project.java
@@ -3,8 +3,8 @@ package Week04Project;
 public class Week04Project {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int testPushInt = 3;
+		System.out.println(testPushInt);
 	}
 
 }
